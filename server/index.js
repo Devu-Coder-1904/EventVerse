9a807799840932da855e22f6415d1e534357d1e0
Divyanshu Garg <divyanshu1904garg@gmail.com>
2026-06-29 21:41:29 +0530

    Connect frontend with Render backend

--- server/index.js
+++ server/index.js
@@ -32,7 +32,17 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/eventora')
 .then(() => { console.log('MongoDB connected'); })
 .catch((err) => { console.error('MongoDB connection error:', err); });
 
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "EventVerse Backend is Live 🚀"
+    });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
